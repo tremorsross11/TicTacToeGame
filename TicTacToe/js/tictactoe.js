509510346e600c291a4bd24f07fdc1687a953e76
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2022-12-05 14:55:33 -0800

    Update tictactoe.js

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -63,3 +63,58 @@ function checkWinConditions() {
         if(a === true && b === true && c === true) {return true;}
     }
 }
+function disableClick() {
+    body.style.pointerEvents = 'none';
+    setTimeout(function() { body.style.pointerEvents = 'auto';}, 1000);
+}
+function audio(audioURL) {
+    let audio = new Audio(audioURL);
+    audio.play();
+}
+function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
+    const canvas = document.getElementById("win-lines");
+    const c = canvas.getContext('2d');
+    let x1 = coordX1,
+    y1 = coordY1,
+    x2 = coordX2,
+    y2 = coordY2,
+    x = x1,
+    y= y1;
+    function animateLineDrawing() {
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+        c.clearRect(0, 0, 608, 608);
+        c.beginPath();
+        c.moveTo(x1, y1);
+        c.lineTo(x, y);
+        c.lineWidth = 10;
+        c.strokeStyle = 'rgba(70, 255, 33, .8)';
+        c.stroke();
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) {x += 10;}
+            if (y < y2) {y += 10;}
+            if (x >= x2 && y >= y2) {cancelAnimationFrame(animationLoop);}
+        }
+        if (x1 <= x2 && y1 >= y2) {
+            if (x < x2) {x += 10;}
+            if (y > y2) {y -= 10;}
+            if (x >= x2 && y <= y2) {cancelAnimationFrame(animationLoop);}
+        }
+    }
+    function clear() {
+        const animationLoop = requestAnimationFrame(clear);
+        c.clearRect(0, 0, 608, 608);
+        cancelAnimationFrame(animationLoop);
+    }
+    disableClick();
+    audio('./media/winGame.mp3');
+    animateLineDrawing();
+    setTimeout(function() { clear(); resetGame(); }, 1000);
+
+}
+function resetGame() {
+    for (let i = 0; i < 9; i++) {
+        let square = document.getElementById(String(i));
+        square.style.backgroundImage = '';
+    }
+    selectedSquares = [];
+}
